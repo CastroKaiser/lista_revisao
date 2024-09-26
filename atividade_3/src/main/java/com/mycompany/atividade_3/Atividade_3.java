b78efcb8f4e7e2f88f0a81daba47ef836cb29d83
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-09-25 21:40:00 -0300

    Add files via upload

---
+++ atividade_3/src/main/java/com/mycompany/atividade_3/Atividade_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade_3;
+
+/**
+ *
+ * @author c.nunes
+ */
+public class Atividade_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
