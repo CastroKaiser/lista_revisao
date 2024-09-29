06009c546dfee6984a55babc64a6668f35b40f4f
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-09-29 14:32:50 -0300

    Add files via upload

---
+++ atividade15/src/main/java/com/mycompany/atividade15/Atividade15.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade15;
+
+/**
+ *
+ * @author edmun
+ */
+public class Atividade15 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
