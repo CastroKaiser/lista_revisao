64a67ad351a93015828797f510436900759aad33
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-09-29 14:38:55 -0300

    Add files via upload

--- atividade14/src/main/java/com/mycompany/atividade14/Atividade14.java
+++ atividade14/src/main/java/com/mycompany/atividade14/Atividade14.java
@@ -8,9 +8,52 @@ package com.mycompany.atividade14;
  *
  * @author edmun
  */
+import java.util.Scanner;
 public class Atividade14 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner ler = new Scanner(System.in);
+        
+        double somaSalariosAtuais = 0;
+        double somaSalariosReajustados = 0;
+
+        while (true) {
+
+            System.out.print("Digite o nome do funcionário (ou FIM para encerrar): ");
+            String nome = ler.nextLine();
+
+  
+            if (nome.equalsIgnoreCase("FIM")) {
+                break;
+            }
+
+            System.out.print("Digite o salário atual de " + nome + ": ");
+            double salarioAtual = ler.nextDouble();
+            ler.nextLine(); 
+
+            double novoSalario = salarioAtual;
+
+            if (salarioAtual <= 150) {
+                novoSalario = salarioAtual * 1.25; 
+            } else if (salarioAtual <= 300) {
+                novoSalario = salarioAtual * 1.20; 
+            } else if (salarioAtual <= 600) {
+                novoSalario = salarioAtual * 1.15; 
+            } else {
+                novoSalario = salarioAtual * 1.10; 
+            }
+
+            System.out.printf("Nome: %s\nSalário atual: R$%.2f\nSalário reajustado: R$%.2f\n\n",nome, salarioAtual, novoSalario);
+
+            somaSalariosAtuais += salarioAtual;
+            somaSalariosReajustados += novoSalario;
+        }
+
+        double diferenca = somaSalariosReajustados - somaSalariosAtuais;
+        System.out.printf("Soma dos salários atuais: R$%.2f\n", somaSalariosAtuais);
+        System.out.printf("Soma dos salários reajustados: R$%.2f\n", somaSalariosReajustados);
+        System.out.printf("Diferença entre salários: R$%.2f\n", diferenca);
+
+        ler.close();
     }
 }
