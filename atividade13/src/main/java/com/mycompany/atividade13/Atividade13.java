9aa4e8f6ebea45ebb7e5172283154723588eaa01
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-09-29 14:31:20 -0300

    Add files via upload

---
+++ atividade13/src/main/java/com/mycompany/atividade13/Atividade13.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade13;
+
+/**
+ *
+ * @author edmun
+ * Imprima todos os números de 150 a 300
+ */
+public class Atividade13 {
+
+    public static void main(String[] args) {
+        for (int i = 150; i < 301; i++) {
+            System.out.print(i+", ");
+        }
+    }
+}
